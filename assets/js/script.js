9f85783c38b6f5da91d792ed766c33017fd85788
VishrutMargam <vishrutm@axioned.com>
2021-11-19 16:53:26 +0530

    added listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ var inputLength = document.querySelector('input[type="number"]');
 var checks = document.querySelectorAll('input[type="checkbox"]');
 var inputPass = document.querySelector('input[type="text"]');
 var copyBtn = document.querySelector('#copy');
+
+
+generateBtn.addEventListener('click', (e) => submitForm(e));
+copyBtn.addEventListener('click', (e) => copyPass(e))
