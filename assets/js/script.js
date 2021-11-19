bef57a9cf5ca18c82b014f1b4299742e4c0c1527
VishrutMargam <vishrutm@axioned.com>
2021-11-19 16:53:12 +0530

    added selectors

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var generateBtn = document.querySelector('#generatePass');
+var inputLength = document.querySelector('input[type="number"]');
+var checks = document.querySelectorAll('input[type="checkbox"]');
+var inputPass = document.querySelector('input[type="text"]');
+var copyBtn = document.querySelector('#copy');
